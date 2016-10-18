c5e8c8d6dc0eb2b38a3128df6fb01662ad080a07
Vassiliy K <hedgeven@gmail.com>
2016-10-18 17:01:11 +0600

    Update find_devices.py

--- find_devices.py
+++ find_devices.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
 from saiman_energy_meter import SaimanEnergyMeter
 
-def main():
+def main(argv):
+
+    if len(argv) < 2:
+        print("Please, specify serial port. Example: %s /dev/ttyUSB0" % argv[0])
+        sys.exit(1)
+
+    serial_port = argv[1]
     for addr in range(1,248):
         try:
             print('%d - ' % addr, end="")
-            if SaimanEnergyMeter(addr, '/dev/ttyACM0'):
+            if SaimanEnergyMeter(addr, serial_port):
                 print("Ok")
         except:
              pass
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
